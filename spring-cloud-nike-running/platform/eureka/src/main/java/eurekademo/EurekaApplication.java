145bb0f795e27cf55bd4ed90985e7867511b9285
sjtugaoman <mangao2015job@gmail.com>
2019-01-28 13:32:36 -0800

    need fix enableEurekaServer bug

--- spring-cloud-nike-running/platform/eureka/src/main/java/eurekademo/EurekaApplication.java
+++ spring-cloud-nike-running/platform/eureka/src/main/java/eurekademo/EurekaApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaApplication {
     public static void main(String[] args) {
         SpringApplication.run(EurekaApplication.class, args);
